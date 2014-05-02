4004309fc193c2dd7666655f5b41b9e386ffc3a5
Craig Gowing (credativ) <craig.gowing@credativ.co.uk>
2014-05-02 09:04:18 +0100

    [IMP] Basic base WMS module which will act as a base for functions/events which could be shared between all modules

---
+++ connector_wms/__openerp__.py
@@ -0,0 +1,53 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    Copyright 2014 credativ Ltd
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+{'name': 'Connector for WMS',
+ 'version': '1.0.0',
+ 'category': 'Connector',
+ 'author': 'credativ Ltd',
+ 'website': 'http://www.credativ.co.uk',
+ 'license': 'AGPL-3',
+ 'description': """
+Connector for WMS
+=================
+
+This modules aims to be a common layer for the connectors dealing with
+Warehouse Management.
+
+It sits on top of the `connector`_ framework and is used by the
+WMS connectors.
+
+That's a technical module, which include amongst other things:
+
+Events
+
+    On which the connectors can subscribe consumers
+    (Picking assigned, ...)
+
+""",
+ 'depends': [
+     'connector',
+     'connector_ecommerce',
+     'delivery',
+ ],
+ 'data': [
+ ],
+ 'installable': True,
+}
---
+++ connector_wms/stock.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    Copyright 2014 credativ Ltd
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+from openerp.osv import orm, fields
+
+from openerp.addons.connector.session import ConnectorSession
+from .event import on_picking_out_available
+
+class stock_picking(orm.Model):
+    _inherit = 'stock.picking'
+
+    def action_assign(self, cr, uid, ids, *args):
+        res = super(stock_picking, self).action_assign(cr, uid, ids, *args)
+        if res:
+            session = ConnectorSession(cr, uid, context=context)
+            picking_records = self.read(cr, uid, ids,
+                                    ['id', 'type'],
+                                    context=context)
+            for picking_vals in picking_records:
+                if picking_vals['type'] != 'out':
+                    continue
+                on_picking_out_available.fire(session, self._name, picking_vals['id'])
+        return res
