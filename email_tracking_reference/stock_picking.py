767c18f32429d7a002eb3a8349370e36e563eddc
Jacob Hicks <jacob.hicks@credativ.co.uk>
2014-07-04 15:07:50 +0100

    [FIX] Re-add required import line

--- email_tracking_reference/stock_picking.py
+++ email_tracking_reference/stock_picking.py
@@ -19,6 +19,7 @@
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
 ##############################################################################
+import connector
 import openerp.addons.connector
 import logging
 from datetime import datetime
