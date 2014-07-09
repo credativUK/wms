9901ca1608a2466e4317baec631f8b45e58e4517
Craig Gowing <craig.gowing@credativ.co.uk>
2014-07-09 13:51:09 +0100

    New examples Script which validates input and converts XML to YaML Script which converts YaML to XML and validates output Updated schemas

--- connector_bots/doc/scripts/validate_xml.py
+++ connector_bots/doc/scripts/validate_xml.py
@@ -1,9 +1,23 @@
 #!/usr/bin/env python
 
-SCHEMA = "../schemas/WarehouseOrderConfirmation_schema_credativ_20140625.xsd"
-DATA = "../examples/WarehouseOrderConfirmation_v1401_DSV.xml"
+#SCHEMA = "../schemas/StockReport_schema_credativ_20140625.xsd"
+#DATA = "../examples/StockReport_20140623145617941_NRI_corrected.xml"
+#DATA = "../examples/StockReport_v1301_DSV.xml"
+
+SCHEMA = "../schemas/WarehouseEntryOrder_schema_credativ_20140625.xsd"
+DATA = "../examples/WarehouseEntryOrder_v1401_DSV.xml"
+
+#SCHEMA = "../schemas/WarehouseOrderConfirmation_schema_credativ_20140625.xsd"
+#DATA = "../examples/WarehouseOrderConfirmation_v1401_DSV.xml"
+
+#SCHEMA = "../schemas/WarehouseShippingOrder_schema_credativ_20140625.xsd"
+#DATA = "../examples/WarehouseShippingOrder_v1401_DSV.xml"
 
 from lxml import etree
+import yaml
+
+def recursive_dict(element):
+    return {element.tag: list(map(recursive_dict, element)) or element.text,}
 
 parser = etree.XMLParser(dtd_validation=True)
 schema_root = etree.parse(SCHEMA)
@@ -11,3 +25,9 @@ schema = etree.XMLSchema(schema_root)
 parser = etree.XMLParser(schema = schema)
 
 data_root = etree.parse(DATA, parser)
+
+data_dicts = recursive_dict(data_root.getroot())
+data_yaml = yaml.dump(data_dicts, default_flow_style=False)
+
+#import ipdb; ipdb.set_trace()
+print data_yaml
